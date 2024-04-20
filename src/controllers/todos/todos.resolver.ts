dae56534b3fc059c1239b6aeed29ed7e8b84d10f
mura-work <murata.naoya.work@gmail.com>
2024-04-20 21:32:48 +0900

    [feat] TodoEntityの作成とクエリによるデータ取得まで

--- src/controllers/todos/todos.resolver.ts
+++ src/controllers/todos/todos.resolver.ts
@@ -1,4 +1,6 @@
+import { Todo } from '@/libs/domain/todo/todo.entity';
 import { PrismaService } from '@/libs/infrastructure/repository/prisma.service';
+import { todoRecordToEntity } from '@/libs/infrastructure/repository/todo/record-to-entity';
 import { TodosModel } from '@/models/todos.model';
 import { TodosService } from '@/services/todos.service';
 import { Args, Query, Resolver } from '@nestjs/graphql';
@@ -11,11 +13,9 @@ export class TodosResolver {
   ) {}
 
   @Query(() => Array(TodosModel))
-  async getTodos(): Promise<TodosModel[]> {
+  async getTodos(): Promise<Todo[]> {
     const todoList = await this.prisma.todo.findMany();
-    console.log(todoList);
-    // return await this.todoService.readAllTodos();
-    return todoList;
+    return todoList.map((r) => todoRecordToEntity(r));
   }
 
   @Query((returns) => TodosModel)
